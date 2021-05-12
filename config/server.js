23e8d1112003f026d4e161503f37000ed975b56c
[your <bkmehta@newageproducts.com>
2021-05-12 11:51:41 +0530

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4c71656cabd26dfbf3392d6d27755423'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ba1220c6-c7e9-48e7-a02e-d7dbe68c7e5e'
+};
